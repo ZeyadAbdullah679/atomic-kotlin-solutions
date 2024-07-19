f15802142d2e28b50d274fa696ed506bfba37fc4
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-19 22:03:40 +0300

    finish summary 2

--- Introduction to Objects/Summary 2/Exercise 5/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 5/src/Task.kt
@@ -1,15 +1,21 @@
 // Summary2/Task5.kt
 package summaryIIExercise5
 
-fun va1() {}
+fun va1(vararg a: Int) {
+    println("va1:")
+    a.forEach {
+        println(it)
+    }
+}
 
-fun va2() {}
+fun va2(vararg a: Int) {
+    println("va2:")
+    va1(*a)
+}
 
 fun main() {
-/*
   va1(1, 2, 3)
   va2(4, 5, 6)
-*/
 }
 /* Expected output:
 va1:
--- Introduction to Objects/Summary 2/Exercise 8/src/Task.kt
+++ Introduction to Objects/Summary 2/Exercise 8/src/Task.kt
@@ -1,16 +1,46 @@
 // Summary2/Task8.kt
 package summaryIIExercise8
+
 import atomictest.eq
 
-val romanNumeralToIntMap: Map<String, Int> = TODO()
+val romanNumeralToIntMap: Map<String, Int> = mapOf(
+    "I" to 1,
+    "IV" to 4,
+    "V" to 5,
+    "IX" to 9,
+    "X" to 10,
+    "XL" to 40,
+    "L" to 50,
+    "XC" to 90,
+    "C" to 100,
+    "CD" to 400,
+    "D" to 500,
+    "CM" to 900,
+    "M" to 1000
+)
 
 fun convertFromRoman(roman: String): Int {
-  TODO()
+    var count = 0
+    var i = 0
+    while (i < roman.length) {
+        val currentValue = when {
+            i < roman.lastIndex && romanNumeralToIntMap.containsKey(roman.substring(i, i + 2)) -> {
+                romanNumeralToIntMap.getValue(roman.substring(i, i + 2)).also {
+                    i++
+                }
+            }
+
+            else -> romanNumeralToIntMap.getValue(roman.substring(i, i + 1))
+        }
+        count += currentValue
+        i++
+    }
+    return count
 }
 
 fun main() {
-  convertFromRoman("XXIII") eq 23
-  convertFromRoman("XLIV") eq 44
-  convertFromRoman("C") eq 100
-  convertFromRoman("MMXVIII") eq 2018
+    print(convertFromRoman("XXIII") eq 23)
+    print(convertFromRoman("XLIV") eq 44)
+    print(convertFromRoman("C") eq 100)
+    print(convertFromRoman("MMXVIII") eq 2018)
 }
