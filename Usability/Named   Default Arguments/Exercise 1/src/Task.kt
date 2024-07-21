f2a12f9013eeb415660c5ecd60e6a3323fd780a4
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-22 00:24:07 +0300

    finish named argument

--- Usability/Named   Default Arguments/Exercise 1/src/Task.kt
+++ Usability/Named   Default Arguments/Exercise 1/src/Task.kt
@@ -1,10 +1,32 @@
 // NamedAndDefaultArgs/Task1.kt
 package namedAndDefaultArgumentsExercise1
 
-class Rectangle
+
+class Rectangle(
+    val side1: Double = 1.0,
+    val side2: Double = 2.0,
+    val color: String = "yellow"
+) {
+    override fun toString(): String {
+        return "$color rectangle $side1 x $side2"
+    }
+}
 
 fun main() {
-  println(Rectangle())
+    println(
+        Rectangle(
+            side1 = 1.0,
+            side2 = 2.0,
+            color = "yellow"
+        )
+    )
+    println(
+        Rectangle(
+            1.0,
+            2.0,
+            color = "yellow"
+        )
+    )
 
-  // TODO
+    // TODO
 }
