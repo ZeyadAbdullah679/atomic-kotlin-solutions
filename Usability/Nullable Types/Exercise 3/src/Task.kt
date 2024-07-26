d7d9c1e34790aac0962ac9d88277b419f8afbcbf
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-27 00:44:25 +0300

    finish nullable types

--- Usability/Nullable Types/Exercise 3/src/Task.kt
+++ Usability/Nullable Types/Exercise 3/src/Task.kt
@@ -1,18 +1,28 @@
 // NullableTypes/Task3.kt
 package nullableTypesExercise3
+
 import atomictest.eq
 
 fun countHexadecimalNumbers(codes: List<String>): Map<Int, Int> {
-  TODO()
+    val map = mutableMapOf<Int, Int>()
+    for (code in codes) {
+        code.toIntOrNull(16)?.let {
+            map[it] = (map[it] ?: 0) + 1
+        }
+    }
+    return map
 }
 
 fun main() {
-  countHexadecimalNumbers(listOf(
-    "1", "3", "7", "8", "A", "B", "F")) eq
-    mapOf(1 to 1, 3 to 1, 7 to 1, 8 to 1, 10 to 1, 11 to 1, 15 to 1)
+    countHexadecimalNumbers(
+        listOf(
+            "1", "3", "7", "8", "A", "B", "F"
+        )
+    ) eq
+            mapOf(1 to 1, 3 to 1, 7 to 1, 8 to 1, 10 to 1, 11 to 1, 15 to 1)
 
-  countHexadecimalNumbers(listOf("1A", "1A", "1A")) eq
-    mapOf(26 to 3)
+    countHexadecimalNumbers(listOf("1A", "1A", "1A")) eq
+            mapOf(26 to 3)
 
-  countHexadecimalNumbers(listOf("Q")) eq mapOf()
+    countHexadecimalNumbers(listOf("Q")) eq mapOf()
 }
