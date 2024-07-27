7f4dc53691a1d74e14f419c6108a4b6284e54944
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-27 03:39:53 +0300

    finish extensions for nullable

--- Usability/Extensions for Nullable Types/Exercise 3/src/Task.kt
+++ Usability/Extensions for Nullable Types/Exercise 3/src/Task.kt
@@ -1,19 +1,19 @@
 // NullableExtensions/Task3.kt
 package extensionsForNullableTypesExercise3
+
 import atomictest.eq
 
 data class Container(val contents: Any?)
 
-// TODO Implement 'empty' and 'full' extension functions
+fun Container?.empty(): Boolean = if (this != null) contents == null else true
+fun Container?.full(): Boolean = if (this != null) contents != null else false
 
 fun main() {
-/*
-  val container = Container(42)
-  container.empty() eq false
-  container.full() eq true
+    val container = Container(42)
+    container.empty() eq false
+    container.full() eq true
 
-  val emptyContainer = Container(null)
-  emptyContainer.empty() eq true
-  emptyContainer.full() eq false
-*/
+    val emptyContainer = Container(null)
+    emptyContainer.empty() eq true
+    emptyContainer.full() eq false
 }
