cf8c282c780a0ea2fcee2fa31cf71835fccfe18e
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-27 04:19:32 +0300

    finish break and continue

--- Usability/break   continue/Exercise 1/src/Task.kt
+++ Usability/break   continue/Exercise 1/src/Task.kt
@@ -1,14 +1,23 @@
 // BreakAndContinue/Task1.kt
 package breakAndContinueExercise1
+
 import atomictest.*
 
 fun readNumbers(vararg n: String) {
-  TODO()
+    var sum = 0
+    for (i in n) {
+        when (i) {
+            in "1".."9" -> sum += i.toInt()
+            "0" -> break
+            else -> trace("Not a number: $i")
+        }
+    }
+    trace("Sum: $sum")
 }
 
 fun main() {
-  readNumbers("1", "a", "3", "10", "0", "19")
-  trace eq """
+    readNumbers("1", "a", "3", "10", "0", "19")
+    trace eq """
     Not a number: a
     Sum: 14
   """
