020581d805d514333264b303b034bd54579bd853
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-27 04:03:29 +0300

    finish extension property

--- Usability/Extension Properties/Exercise 2/src/Task.kt
+++ Usability/Extension Properties/Exercise 2/src/Task.kt
@@ -1,12 +1,19 @@
 // ExtensionProperties/Task2.kt
 package extensionPropertiesExercise2
+
 import atomictest.eq
 
-// TODO: implement 'reversed'
+val <T> List<T>.reversed: List<T>
+    get() {
+        val reversed = mutableListOf<T>()
+        for (i in lastIndex downTo 0)
+            reversed.add(this[i])
+        return reversed
+    }
 
 fun main() {
-/*
-  val list = listOf(1, 2, 3)
-  list.reversed eq listOf(3, 2, 1)
-*/
+    /*
+      val list = listOf(1, 2, 3)
+      list.reversed eq listOf(3, 2, 1)
+    */
 }
