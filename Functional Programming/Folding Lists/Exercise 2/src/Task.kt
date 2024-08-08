7f65ec57c9ac064963c4131c570743e028004139
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-08-08 11:18:25 +0300

    finish fold lists

--- Functional Programming/Folding Lists/Exercise 2/src/Task.kt
+++ Functional Programming/Folding Lists/Exercise 2/src/Task.kt
@@ -1,11 +1,12 @@
 // FoldingLists/Task2.kt
 package foldingListsExercise2
+
 import atomictest.eq
 
 fun <T> List<T>.count(predicate: (T) -> Boolean): Int =
-  TODO("fold(???) { ??? }")
+    fold(0) {acc, t -> if(predicate(t)) acc + 1 else acc }
 
 fun main() {
-  val list = listOf(1, -2, 3)
-  list.count { it > 0 } eq 2
+    val list = listOf(1, -2, 3)
+    list.count { it > 0 } eq 2
 }
