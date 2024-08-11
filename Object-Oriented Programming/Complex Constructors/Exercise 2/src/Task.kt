d6ee83c6ce89313d9a318b714e07f4d783f3e3ba
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-08-11 09:29:35 +0300

    finish complex constructors

--- Object-Oriented Programming/Complex Constructors/Exercise 2/src/Task.kt
+++ Object-Oriented Programming/Complex Constructors/Exercise 2/src/Task.kt
@@ -1,9 +1,14 @@
 // ComplexConstructors/Exercise2.kt
 package complexConstructorsExercise2
-import atomictest.eq
 
-class SumChars
+class SumChars(text: String) {
+    val sum: Int
+
+    init {
+        sum = text.chars().sum()
+    }
+}
 
 fun main() {
-  // SumChars("What?").sum eq 467
+    // SumChars("What?").sum eq 467
 }
--- Object-Oriented Programming/Complex Constructors/Exercise 3/src/Task.kt
+++ Object-Oriented Programming/Complex Constructors/Exercise 3/src/Task.kt
@@ -1,20 +1,24 @@
 // ComplexConstructors/Exercise3.kt
 package complexConstructorsExercise3
+
 import atomictest.eq
 
 class MultipleInit {
-  val initOrder = mutableListOf<String>()
-  init {
-    // TODO
-  }
-  init {
-    // TODO
-  }
-  init {
-    // TODO
-  }
+    val initOrder = mutableListOf<String>()
+
+    init {
+        initOrder.add("one")
+    }
+
+    init {
+        initOrder.add("two")
+    }
+
+    init {
+        initOrder.add("three")
+    }
 }
 
 fun main() {
-  MultipleInit().initOrder eq "[one, two, three]"
+    MultipleInit().initOrder eq "[one, two, three]"
 }
